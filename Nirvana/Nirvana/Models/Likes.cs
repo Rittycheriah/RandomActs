d49fc0d8591f3b7e41731be85538e417ca1f257c
unknown <rittycheriah@gmail.com>
2015-11-26 12:52:16 -0600

    Like model added & tested

---
+++ Nirvana/Nirvana/Models/Likes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nirvana.Models
+{
+    public class Likes
+    {
+        public ApplicationUser User { get; set; }
+        public bool Liked { get; set; }
+        public int ActId { get; set; }
+    }
+}
---
+++ Nirvana/NirvanaTests/ModelTests/LikeModelTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Nirvana.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NirvanaTests.ModelTests
+{
+    [TestClass]
+    public class LikeModelTest
+    {
+        [TestMethod]
+        public void LikeModelEnsureICanCreateAnInstance()
+        {
+            Likes _like = new Likes();
+            Assert.IsNotNull(_like);
+        }
+
+        [TestMethod]
+        public void LikeModelEnsurePropertiesWork()
+        {
+            RandomActsModel act = new RandomActsModel { RandomActId = 1};
+            Likes _like = new Likes();
+            _like.Liked = true;
+            _like.ActId = act.RandomActId;
+
+            Assert.AreEqual(true, _like.Liked);
+            Assert.AreEqual(1, _like.ActId);
+        }
+    }
+}
