5ca002e78b8cd68373044372174aa221d01b0de2
unknown <rittycheriah@gmail.com>
2015-11-26 13:08:55 -0600

    finished rank model & tests

---
+++ Nirvana/Nirvana/Models/Rank.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nirvana.Models
+{
+    public class Rank
+    {
+        public int Rank_Id { get; set; }
+        public int Rank_Code { get; set; }
+        public string Name { get; set; }
+        public bool CommentFeat { get; set; }
+        public bool SocialMedia { get; set; }
+        public int BasePtsAllowance { get; set; }
+        public int MinimumPtReq { get; set; }
+    }
+}
---
+++ Nirvana/NirvanaTests/ModelTests/RankModelTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Nirvana.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NirvanaTests.ModelTests
+{
+    [TestClass]
+    public class RankModelTests
+    {
+        [TestMethod]
+        public void RankEnsureICanCreateAnInstance()
+        {
+            Rank _rank = new Rank();
+            Assert.IsNotNull(_rank);
+        }
+
+        [TestMethod]
+        public void RankEnsurePropertiesWork()
+        {
+            Rank _rank = new Rank { Rank_Id = 1, BasePtsAllowance = 3, CommentFeat = false, MinimumPtReq = 10, SocialMedia = false, Name = "Grasshopper", Rank_Code = 1};
+
+            Assert.AreEqual(1, _rank.Rank_Id);
+            Assert.AreEqual(1, _rank.Rank_Code);
+            Assert.AreEqual("Grasshopper", _rank.Name);
+            Assert.AreEqual(false, _rank.CommentFeat);
+            Assert.AreEqual(false, _rank.SocialMedia);
+            Assert.AreEqual(3, _rank.BasePtsAllowance);
+            Assert.AreEqual(10, _rank.MinimumPtReq);
+        }
+    }
+}
