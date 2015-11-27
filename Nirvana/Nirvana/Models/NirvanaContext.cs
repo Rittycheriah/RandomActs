aefc1d6fd14aef31485ca543c4f9de5159008c07
unknown <rittycheriah@gmail.com>
2015-11-27 11:46:34 -0600

    created context, need web.config string

---
+++ Nirvana/Nirvana/Models/NirvanaContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Nirvana.Models
+{
+    public class NirvanaContext
+    {
+        public virtual DbSet<RandomActsModel> Acts { get; set; }
+        public virtual IDbSet<Comment> Comments { get; set; }
+        public virtual IDbSet<Rank> Ranks { get; set; }
+        public virtual IDbSet<Likes> Likes { get; set; }
+    }
+}
